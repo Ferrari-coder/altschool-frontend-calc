9c3292fcb2e9cf240e7fab56a9fd3fbb369e2e10
Oluwaferanmi Aiyegbusi <oluwaferanmiaiyegbusi@gmail.com>
2022-10-08 21:58:41 +0100

    almost done

--- assignment2/src/calculator.js
+++ assignment2/src/calculator.js
@@ -1,38 +1,76 @@
 import React from "react";
+import { useState } from "react";
 import "./style.css";
 
 const Calculator = () => {
+  const [calc, setCalc ] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const oper = ['/', '*','+','-','.','√']
+
+  const updateCalc = value => {
+    if (
+      oper.includes(value) && calc === '' ||
+      oper.includes(value) && oper.includes(calc.slice(-1))
+    ) {
+      return;
+    } 
+    setCalc(calc + value);
+
+    if(!oper.includes(value)){
+      setAnswer(eval(calc + value).toString()); 
+    }
+  }
+   
+ 
+    const caluculation = () => {
+      setCalc(eval(calc).toString());
+    }
+
+    const del = () => {
+      if (calc === ''){
+        return;
+      }
+
+      const value = calc.slice(0, -1);
+      setCalc(value);
+    }
+      // for ac
+      const reset = () => {
+        setAnswer("")
+        setCalc("")
+      }
   return (
     <section className="main-container">
       <h2>React Calculator</h2>
       <div className="container">
         <div className="answer">
-            <p className="question">04--0-</p>
-            <p className="result">1234r4</p>
+            <p className="question">{calc || "0"}</p>
+            <p className="result">{answer ? <span>{answer}</span>: ''}</p>
         </div>
         <div className="buttons">
-          <span class="button clear-all first-row">AC</span>
-          <span class="button modulo first-row">%</span>
-          <span class="button delete first-row"><i class="fa-solid fa-delete-left"></i></span>
-          <span class="button square first-row">x<sup>2</sup></span>
-          <span class="button inverse del"><sup>1</sup>/x</span>
-          <span class="button square-root del">&radic;</span>
-          <span class="button operator">/</span>
-          <span class="button operator">*</span>
-          <span class="button number">7</span>
-          <span class="button number">8</span>
-          <span class="button number">9</span>
-          <span class="button operator">-</span>
-          <span class="button number">4</span>
-          <span class="button number">5</span>
-          <span class="button number">6</span>
-          <span class="button operator biggi">+</span>
-          <span class="button number">1</span>
-          <span class="button number">2</span>
-          <span class="button number">3</span>
-          <span class="button number zero">0</span>
-          <span class="button number point">.</span>
-          <span class="button operator equals">=</span>
+          <span className="button clear-all first-row" onClick={reset}>AC</span>
+          <span className="button bracket first-row" onClick={() => updateCalc('(')}>(</span>
+          <span className="button bracket first-row" onClick={() => updateCalc(')')}>)</span>
+          <span className="button delete first-row" onClick={del}><i class="fa-solid fa-delete-left"></i></span>
+          <span className="button square del" onClick={() => updateCalc('**2')}>x<sup>2</sup></span>
+          <span className="button square-root del" onClick={() => updateCalc('**(1/2)')}>&radic;</span>
+          <span className="button operator" onClick={() => updateCalc('/')}>/</span>
+          <span className="button operator" onClick={() => updateCalc('*')}>*</span>
+          <span className="button number" onClick={() => updateCalc('7')}>7</span>
+          <span className="button number" onClick={() => updateCalc('8')}>8</span>
+          <span className="button number" onClick={() => updateCalc('9')}>9</span>
+          <span className="button operator" onClick={() => updateCalc('-')}>-</span>
+          <span className="button number" onClick={() => updateCalc('4')}>4</span>
+          <span className="button number" onClick={() => updateCalc('5')}>5</span>
+          <span className="button number" onClick={() => updateCalc('6')}>6</span>
+          <span className="button operator biggi" onClick={() => updateCalc('+')}>+</span>
+          <span className="button number" onClick={() => updateCalc('1')}>1</span>
+          <span className="button number" onClick={() => updateCalc('2')}>2</span>
+          <span className="button number" onClick={() => updateCalc('3')}>3</span>
+          <span className="button number zero" onClick={() => updateCalc('0')}>0</span>
+          <span className="button number point" onClick={() => updateCalc('.')}>.</span>
+          <span className="button operator equals" onClick={caluculation}>=</span>
         </div>
       </div>
     </section>
